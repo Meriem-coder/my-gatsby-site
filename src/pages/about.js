21262fb40f747a332404569839b5e286e5d01717
unknown <votre@email.com>
2023-12-24 14:45:21 +0100

    Finished Gatsby Tutorial Part 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 // Step 1: Import React
-import * as React from 'react'
+/*import * as React from 'react'
 
 // Step 2: Define your component
 const AboutPage = () => {
@@ -13,4 +13,19 @@ const AboutPage = () => {
 export const Head = () => <title>About Me</title>
 
 // Step 3: Export your component
+export default AboutPage*/
+
+import * as React from 'react'
+import Layout from '../component/Layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+/*import * as React from 'react'
 import { Link } from 'gatsby'
 
 const IndexPage = () => {
@@ -9,6 +9,20 @@ const IndexPage = () => {
       <a href="/about">About</a>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
+  )
+}
+
+export const Head = () => <title>Home Page</title>
+
+export default IndexPage*/
+import * as React from 'react'
+import Layout from '../component/Layout'
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>Hello 2IAD.</p>
+    </Layout>
   )
 }
 
